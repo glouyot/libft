11d1d553fffd6147fe090149e9e84cc153446785
glouyot <glouyot@student.42.fr>
2016-12-13 19:36:30 +0100

    on tien un truc

--- srcs/convert/ft_itoa_base.c
+++ srcs/convert/ft_itoa_base.c
@@ -6,7 +6,7 @@
 /*   By: glouyot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/13 16:39:34 by glouyot           #+#    #+#             */
-/*   Updated: 2016/12/13 18:09:02 by glouyot          ###   ########.fr       */
+/*   Updated: 2016/12/13 19:36:19 by glouyot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,19 +22,24 @@ char				*ft_itoa_base(int n, char *base)
 	l = n;
 	i = 1;
 	neg = (l < 0);
-	while (ft_pow(ft_strlen(base), i) <= n)
+	DEBUG;
+	while (ft_pow(ft_strlen(base), i) <= (neg ? -l : l))
+	{
+		ft_putnbr(i);
+		ft_putendl("");
 		i++;
-	ret = ft_strnew(8);
+	}
+	DEBUG;
+	ret = (neg) ? ft_strnew(8 * (i / 8 + 1)) : ft_strnew(i);
 	if (!ret)
 		return (NULL);
 	if (neg)
 	{
-		l = -l;
-		ret[0] = '-';
+		i = (8 * (i / 8 + 1));
 	}
 	while (i > 0)
 	{
-		ret[i + neg - 1] = base[l % ft_strlen(base)];
+		ret[i - 1] = base[l % ft_strlen(base)];
 		i--;
 		l = l / ft_strlen(base);
 	}
