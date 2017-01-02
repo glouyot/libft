30b0d30691addfc3f7fe0dbc9042593247e521c2
glouyot <glouyot@student.42.fr>
2017-01-02 13:01:06 +0100

    maj tree

--- includes/ft_btree.h
+++ includes/ft_btree.h
@@ -6,7 +6,7 @@
 /*   By: glouyot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/29 13:02:34 by glouyot           #+#    #+#             */
-/*   Updated: 2016/12/29 15:02:20 by glouyot          ###   ########.fr       */
+/*   Updated: 2017/01/02 12:55:03 by glouyot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,9 @@ typedef struct		s_btree
 	struct s_btree	*right;
 }					t_btree;
 
-t_btree				*ft_btree_create_node(void *item);
+t_btree				*ft_btree_create_node(void const *item);
+void				ft_btree_apply_prefix(t_btree *root, void (*f)(void *));
+void				ft_btree_apply_suffix(t_btree *root, void (*f)(void *));
+void				ft_btree_apply_infix(t_btree *root, void (*f)(void *));
 
 #endif
