ecfaec14c2a611329e9b3a6875b8da3de601789a
glouyot <glouyot@student.42.fr>
2017-01-03 11:29:07 +0100

    Ajout rb tree struct

--- includes/ft_btree.h
+++ includes/ft_btree.h
@@ -6,23 +6,38 @@
 /*   By: glouyot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/29 13:02:34 by glouyot           #+#    #+#             */
-/*   Updated: 2017/01/02 12:55:03 by glouyot          ###   ########.fr       */
+/*   Updated: 2017/01/03 11:28:19 by glouyot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_BTREE_H
 # define FT_BTREE_H
 
-typedef struct		s_btree
+enum					e_rb_color
 {
-	void			*item;
-	struct s_btree	*left;
-	struct s_btree	*right;
-}					t_btree;
+	RB_BLACK,
+	RB_RED
+};
 
-t_btree				*ft_btree_create_node(void const *item);
-void				ft_btree_apply_prefix(t_btree *root, void (*f)(void *));
-void				ft_btree_apply_suffix(t_btree *root, void (*f)(void *));
-void				ft_btree_apply_infix(t_btree *root, void (*f)(void *));
+typedef struct			s_rb_node
+{
+	struct s_rb_node	*parent;
+	struct s_rb_node	*left;
+	struct s_rb_node	*right;
+	void				*data;
+	enum				color;
+}						t_rb_node;
+
+typedef struct			s_btree
+{
+	void				*item;
+	struct s_btree		*left;
+	struct s_btree		*right;
+}						t_btree;
+
+t_btree					*ft_btree_create_node(void const *item);
+void					ft_btree_apply_prefix(t_btree *root, void (*f)(void *));
+void					ft_btree_apply_suffix(t_btree *root, void (*f)(void *));
+void					ft_btree_apply_infix(t_btree *root, void (*f)(void *));
 
 #endif
--- srcs/btree/ft_btree_apply.c
+++ srcs/btree/ft_btree_apply.c
@@ -6,7 +6,7 @@
 /*   By: glouyot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/29 14:29:59 by glouyot           #+#    #+#             */
-/*   Updated: 2016/12/29 15:30:15 by glouyot          ###   ########.fr       */
+/*   Updated: 2017/01/02 13:16:40 by glouyot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,11 @@ void	ft_btree_apply_prefix(t_btree *root, void (*f)(void *))
 {
 	if (root)
 		(*f)(root->item);
+	else
+	{
+		ft_error("Tree is NULL in apply_prefix");
+		return ;
+	}
 	if (root->left != NULL)
 		ft_btree_apply_prefix(root->left, (*f));
 	if (root->right != NULL)
@@ -30,6 +35,11 @@ void	ft_btree_apply_suffix(t_btree *root, void (*f)(void *))
 		ft_btree_apply_suffix(root->right, (*f));
 	if (root)
 		(*f)(root->item);
+	else
+	{
+		ft_error("Tree is NULL in apply_suffix");
+		return ;
+	}
 }
 
 void	ft_btree_apply_infix(t_btree *root, void (*f)(void *))
@@ -38,6 +48,11 @@ void	ft_btree_apply_infix(t_btree *root, void (*f)(void *))
 		ft_btree_apply_infix(root->left, (*f));
 	if (root)
 		(*f)(root->item);
+	else
+	{
+		ft_error("Tree is NULL in apply_infix");
+		return ;
+	}
 	if (root->right != NULL)
 		ft_btree_apply_infix(root->right, (*f));
 }
